5738089d24e2a07ad331a55981fbc2d577336a1c
thyanmol <anmoljain232606@gmail.com>
2020-05-16 19:54:25 +0530

    Application Built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+import {SearchBar, VideoDetail, VideoList} from './component';
+
+import youtube from './api/youtube';
+
 class App extends React.Component{
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit('anmol jain harmonica');
+    }
+
+    onVideoSelect= (video) => {
+        this.setState({selectedVideo : video}); 
+    }
+
+    handleSubmit = async (searchTerm) => {
+        
+        const response = await youtube.get('search', { 
+            params: {
+                part: 'snippet',
+                maxResults : 5,
+                key : 'AIzaSyD2qoZD2rU15hPuiR_YoddKH9LpxDLfpZQ',
+                q : searchTerm,
+            }
+        });
+
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    }
+
     render(){
+        const { selectedVideo, videos} = this.state;
+
         return(
-            <h1> Youtube Clone </h1>
+
+            <Grid justify ="center" container spacing ={10}>                
+                <Grid item xs = {12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs = {12}>
+                           <SearchBar onFormSubmit = {this.handleSubmit}/>
+                        </Grid>
+                        
+                        <Grid item xs = {8}>
+                            <VideoDetail video={selectedVideo}/>           
+                        </Grid>
+
+                        <Grid item xs = {4}>
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>                 
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            
         )
     }
 }
